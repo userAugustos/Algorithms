c0bbe6cf9ad6dd38a9b5079f1bda97e3f68263df
userAugustos <felipe_augustos84@outlook.com.br>
2022-12-03 10:23:20 -0300

    feat: some thoughts about promises in js, and ofcourse, explanations

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+E
+sort/selectionSort.js,b/c/bc62aa0916669f4b7fae92bde110143db1cc0ea2
+F
+freeCodeCamp/stacks.js,0/6/069e9f2b508610e3e04c89e37df17cdbcb67d834
+=
+palindrome.js,c/3/c37a19eb1a089a1c43129e50fb06613fc073cdb2
--- promises.js
+++ promises.js
@@ -45,14 +45,15 @@ let promise = new Promise((resolve, reject) => {
 
 /**
  * so u can undertans that, with a promise we can have a resolve and a reject as states of your callbacks, then, resolveds are in the "then" and reject in "catch"
- * as i think about promises, the thing stands as this simple pattern
+ * as I think about promises, the thing stands as this simple pattern
   */
-promise.then((result) => {
+const promise2 = promise.then((result) => {
     console.log("Sucesso é um número " + result)
 }).catch((err) => {
     console.log("Eita, é um número " + err)
 })
 
+console.debug(promise2)
 /**
  * Sync function
  * another example...
@@ -75,4 +76,41 @@ function accessToCompanyDash(callback, errorCallback) {
 // so this is a simple sync function, but if you run now, u will se that this func loggs come before the promise above
 accessToCompanyDash((result) => console.log("Acesso Liberado", result), (err) => console.log("Eita, negado", err))
 
+// now as async
 
+const companyPromise = (isCompany) => {
+    return new Promise((resolve, reject) => {
+        if(!isCompany) {
+            reject({
+                allowed: false,
+                name: undefined,
+            })
+        }else {
+            resolve({
+                allowed: true,
+                name: "Facebook",
+            })
+        }
+    })
+}
+
+// here im passing catch inside .then
+companyPromise(isCompany).then((result) => console.log("Acesso Liberado", result), (err) => console.log("Eita Negado", err))
+
+
+// Let's se a chaining after catch example with promise
+
+const url = "https://jsonplaceholder.typicode.com/users";
+
+// the fetch api is a promise
+
+let users;
+
+fetch(url)
+    .then(res => res.json())
+    .then(data => users = data)
+    .catch((err) => console.log("Error", err))
+    .then(() => users = undefined)
+
+// so above if the fetch fails for some reason, uor app doesn't throw exception, just do another thing with the error, then we can keep work in the promise
+console.log(users)
